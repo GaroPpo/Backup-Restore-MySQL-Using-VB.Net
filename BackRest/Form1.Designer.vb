73ab3332acf5181db09eac79230c2a13cfa1217e
Eric Liputra <ericliputra95@gmail.com>
2017-10-25 10:10:04 +0800

    Initial Commit
    Initial Commit to GitHub

---
+++ BackRest/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.FolderBrowserDialog1 = New System.Windows.Forms.FolderBrowserDialog()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 37)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Backup"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(197, 12)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 37)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Restore"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 61)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Backup Restore"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+End Class
---
+++ BackRest/Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Dim Path As String
+    Dim BackupPath As String
+    Dim DatabaseName As String = "Backup " + Date.Now.ToString("dd-MM-yyyy HH-mm-ss")
+
+    Sub Backup()
+        Try
+            If Not Directory.Exists(BackupPath) Then
+                Directory.CreateDirectory(BackupPath)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+        Process.Start("C:\wamp64\bin\mysql\mysql5.7.14\bin\mysqldump.exe", "-u root skripsi -r """ & BackupPath & "" & DatabaseName & ".sql""")
+        Process.Start("C:\xampp\mysql\bin\mysqldump.exe", "-u root skripsi -r """ & BackupPath & "" & DatabaseName & ".sql""")
+        MsgBox("Backup Created Successfully!", MsgBoxStyle.Information, "Backup")
+    End Sub
+    Sub Restore()
+        Dim myProcess As New Process()
+
+        myProcess.StartInfo.FileName = "cmd.exe"
+        myProcess.StartInfo.UseShellExecute = False
+        myProcess.StartInfo.WorkingDirectory = "C:\wamp64\bin\mysql\mysql5.7.14\bin"
+        myProcess.StartInfo.WorkingDirectory = "C:\xampp\mysql\bin\"
+        myProcess.StartInfo.RedirectStandardInput = True
+        myProcess.StartInfo.RedirectStandardOutput = True
+        myProcess.Start()
+        Dim myStreamerWriter As StreamWriter = myProcess.StandardInput
+        Dim myStreamerReader As StreamReader = myProcess.StandardOutput
+        myStreamerWriter.WriteLine("mysql -u root skripsi < " & Path & "")
+        myStreamerWriter.Close()
+        myProcess.WaitForExit()
+        myProcess.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        FolderBrowserDialog1.ShowDialog()
+        BackupPath = FolderBrowserDialog1.SelectedPath.ToString() + "\"
+        Backup()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        OpenFileDialog1.Title = "Please Select a File"
+        OpenFileDialog1.InitialDirectory = "C:\"
+        OpenFileDialog1.ShowDialog()
+    End Sub
+
+    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
+        Path = OpenFileDialog1.FileName.ToString()
+        Restore()
+        MsgBox("Database Restoration Successfully!", MsgBoxStyle.Information, "Restore")
+    End Sub
+End Class
